29378ca3651ca1859b4d3af63da07a657792a583
Anton Anuchin <anton-anuchin@mail.ru>
2022-08-01 10:21:51 +0300

    yamdb_final

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['158.160.0.12', '127.0.0.1', 'localhost']
 
 AUTH_USER_MODEL = 'reviews.User'
 
